aa4f99b7af6d5deb30cf478720afcac3f01c837d
Ww022 <thuanvhps38410@gmail.com>
2024-05-13 20:51:47 +0700

    UI

---
+++ Assets/UI/chucnang.cs
@@ -0,0 +1,16 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class chucnang : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void Quit()
+    {
+
+    }
+
+}
