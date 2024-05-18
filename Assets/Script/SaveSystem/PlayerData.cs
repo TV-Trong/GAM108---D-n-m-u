4e24d38925e21922aa0276650bf54e9a64a50c05
TV-Trong <trongtvps38376@fpt.edu.vn>
2024-05-18 23:05:29 +0700

    Hệ thống lưu người chơi (chưa hoàn thành)

--- Assets/Script/SaveSystem/PlayerData.cs
+++ Assets/Script/SaveSystem/PlayerData.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour
+public class PlayerData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string username;
+    public int health;
+    public int coin;
+    public int soul;
+    public Vector2 pos;
+    public DateTime timeCreated;
+    public float timePlayed;
 
-    // Update is called once per frame
-    void Update()
+    public PlayerData (string name, DateTime time)
     {
-        
+       username = name;
+       timeCreated = time;
     }
 }
